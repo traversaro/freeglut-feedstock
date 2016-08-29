1c3c52348bf07b15c7ef79ec19f0d3bd0607f309
shadow_walker <shadowwalkersb@gmail.com>
2016-08-29 00:50:26 -0500

    add bld.bat

---
+++ recipe/bld.bat
@@ -0,0 +1,14 @@
+if "%ARCH%" == "64" (
+  set DIRECTORY=x64
+) else (
+  set DIRECTORY=.
+)
+
+mkdir build && cd build
+cmake ^
+	-DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%  ^
+	-DCMAKE_INSTALL_BINDIR=%LIBRARY_BIN%     ^
+	-DCMAKE_INSTALL_LIBDIR=%LIBRARY_LIB%     ^
+	-DCMAKE_INSTALL_INCLUDEDIR=%LIBRARY_INC% ^
+	-DFREEGLUT_BUILD_DEMOS=OFF               ^
+	..
