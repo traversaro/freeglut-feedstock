5195acedc6701320b82fff1f4157e3aec3534f1d
shadow_walker <shadowwalkersb@gmail.com>
2016-09-02 23:21:05 -0500

    add build features, use CMAKE_GENERATOR

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,6 @@
 mkdir build && cd build
 cmake ^
+	-G "%CMAKE_GENERATOR%"                   ^
 	-DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%  ^
 	-DCMAKE_INSTALL_BINDIR=%LIBRARY_BIN%     ^
 	-DCMAKE_INSTALL_LIBDIR=%LIBRARY_LIB%     ^
