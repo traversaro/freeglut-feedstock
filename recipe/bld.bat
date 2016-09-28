888a4700678fd50e67bff05f81396caaabae1be1
John Kirkham <kirkhamj@janelia.hhmi.org>
2016-09-27 22:38:47 -0400

    recipe: Switch to NMake for the Windows build.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,6 @@
 mkdir build && cd build
 cmake ^
-	-G "%CMAKE_GENERATOR%"                   ^
+	-G "NMake Makefiles"                     ^
 	-DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%  ^
 	-DCMAKE_INSTALL_BINDIR=%LIBRARY_BIN%     ^
 	-DCMAKE_INSTALL_LIBDIR=%LIBRARY_LIB%     ^
