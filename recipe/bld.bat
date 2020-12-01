a7cce310d23e7f05c141f5ec53d3f5ff4709670e
Wolf Vollprecht <w.vollprecht@gmail.com>
2020-12-01 13:11:09 +0100

    hardlink glut.dll and lib to freeglut

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,8 +6,11 @@ cmake ^
 	-DCMAKE_INSTALL_LIBDIR=%LIBRARY_LIB%     ^
 	-DCMAKE_INSTALL_INCLUDEDIR=%LIBRARY_INC% ^
 	-DCMAKE_BUILD_TYPE=Release               ^
-	-DFREEGLUT_REPLACE_GLUT=ON               ^
+	-DFREEGLUT_REPLACE_GLUT=ON              ^
 	-DFREEGLUT_BUILD_DEMOS=OFF               ^
 	..
 
 cmake --build . --config Release --target INSTALL
+
+mklink /h %LIBRARY_BIN%\freeglut.dll %LIBRARY_BIN%\glut.dll
+mklink /h %LIBRARY_LIB%\freeglut.lib %LIBRARY_LIB%\glut.lib
