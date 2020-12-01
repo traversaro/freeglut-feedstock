cc68ac998ebbf49efb0d29c82b1f23bbafe94063
Wolf Vollprecht <w.vollprecht@gmail.com>
2020-12-01 15:20:31 +0100

    remove static library from freeglut

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,6 +7,9 @@ cmake \
 	-DCMAKE_INSTALL_LIBDIR=$PREFIX/lib      \
 	-DCMAKE_BUILD_TYPE=Release              \
 	-DFREEGLUT_BUILD_DEMOS=OFF              \
+	-DFREEGLUT_BUILD_STATIC_LIBS=OFF        \
+	-DFREEGLUT_BUILD_SHARED_LIBS=ON         \
 	..
+
 make
 make install
