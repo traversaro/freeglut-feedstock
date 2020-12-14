47ca6315328916628a7407a625bea61b8b3aaed9
Silvio Traversaro <silvio@traversaro.it>
2020-12-14 19:00:50 +0100

    Update run_test.bat

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -3,7 +3,7 @@ setlocal EnableDelayedExpansion
 cd test
 
 :: Compile and run example that links glut with CMake
-cmake -GNinja -DCMAKE_BUILD_TYPE=Release ..
+cmake -GNinja -DCMAKE_BUILD_TYPE=Release .
 if errorlevel 1 exit 1
 
 cmake --build . --config Release
